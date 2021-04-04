f58733d49a0926b0c6203f9cc0accce4a88cdf29
DorAm <bndor.am@gmail.com>
2021-04-04 15:49:07 +0300

    Palindrome Linked List

---
+++ LinkedList/PalindromeLinkedList.js
@@ -0,0 +1,35 @@
+
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+
+
+function PalindromeLinkedList(head) {
+    let currListNode = head;
+    let currReversedListNode = ReverseLinkedList(head);
+    while (currListNode) {
+        if (currListNode.val !== currReversedListNode.val) {
+            return false;
+        }
+        currListNode = currListNode.next;
+        currReversedListNode = currReversedListNode.next;
+    }
+    return true;
+}
+
+function ReverseLinkedList(head) {
+    let current = head;
+    let tailPointer = undefined;
+    let newNode = null;
+    while (current) {
+        newNode = new ListNode(current.val, tailPointer);
+        tailPointer = newNode;
+        current = current.next;
+    }
+    return newNode;
+}
+
+debugger;
+let list = new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1, undefined))));
+PalindromeLinkedList(list);
